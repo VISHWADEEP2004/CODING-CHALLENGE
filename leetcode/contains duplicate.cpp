0deb1857add1d978de717e7a11b3d49394b29a49
VISHWADEEP2004 <117439533+VISHWADEEP2004@users.noreply.github.com>
2023-11-06 09:46:20 +0530

    Update and rename test.cpp to contains duplicate.cpp

---
+++ leetcode/contains duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                    count++;
+                    break;
+                }   
+        }
+        if(count>0)
+            return true;
+        else
+            return false;
+    }
+};
