f5c70024a55095432e97fa2db2d3ea937b06adc2
VISHWADEEP2004 <117439533+VISHWADEEP2004@users.noreply.github.com>
2023-12-07 22:40:48 +0530

    Create DAY_30_ Sum of dependencies in a graph.cpp

---
+++ geeksforgeeks/DAY_30_ Sum of dependencies in a graph.cpp
@@ -0,0 +1,12 @@
+Sum of dependencies in a graph:
+
+class Solution {
+  public:
+    int sumOfDependencies(vector<int> adj[], int V) {
+         int sum = 0;
+        for (int i = 0; i < V; ++i) {
+            sum += adj[i].size();
+        }
+        return sum;
+    }
+};
